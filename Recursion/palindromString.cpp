b336a338f0b248c6a48951dc8a38d51d2293a933
Md Rony Islam <mdronyislam149@gmail.com>
2025-05-26 23:12:31 +0600

    new added

---
+++ Recursion/palindromString.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+bool palString( int i, string s, int n){
+    if( i >= n){
+        cout<< "This is Palindrom string: ";
+        return true;
+    }
+    if(s[i] != s[n-i-1]){
+        cout<< "This is not: ";
+        return false;
+    }
+    else return palString( i+1, s, n);
+}
+
+int main()
+{
+    string str;
+    cin>> str;
+    int size = str.size();
+    cout << palString(0, str, size);
+    return 0;
+}
