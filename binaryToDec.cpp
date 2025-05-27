311de20c7b3c8c7e02e1058a52764da6ab74241a
Md Rony Islam <mdronyislam149@gmail.com>
2025-05-27 18:24:14 +0600

    Binary to decimal added

---
+++ binaryToDec.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n; 
+    cin>> n;
+    int remain, ans = 0, i =0;
+    while(n != 0){
+        remain = n % 2;
+        n = n /10;
+        ans = ans + remain * (1<<i);
+        i++;
+    }
+    cout << ans;
+    return 0;
+}
