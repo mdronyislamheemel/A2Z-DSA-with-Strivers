bedabde4f9d082325a7b8fde006748c34c42d46c
Md Rony Islam <mdronyislam149@gmail.com>
2025-06-25 01:02:18 +0600

    map added

---
+++ Hashing/numberMap.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+int main()
+{
+    int n;
+    cin>> n;
+    vector<int> arr(n);
+    for( int i = 0; i< n; i++){
+        cin>> arr[i];
+    }
+
+    //precompute
+    map<int, int> mpp;
+    for(int i = 0; i< n; i++){
+        mpp[arr[i]]++;
+    }
+
+    //iterate in the map
+    for(auto it: mpp){
+        cout<< it.first <<"->" << it.second<< endl;
+    }
+
+    int q; //number of queries
+    cin >> q;
+    while (q--)
+    {
+        int number;
+        cin>> number;
+        //fetch
+        cout<< mpp[number]<< endl;
+    }
+    
+    return 0;
+}
