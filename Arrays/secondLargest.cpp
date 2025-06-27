6c6b20899d48b3908e5b7656e90d07a0804a8942
Md Rony Islam <mdronyislam149@gmail.com>
2025-06-27 16:46:47 +0600

    something new added

---
+++ Arrays/secondLargest.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+//aita just largest number ber korar method
+int largestf(vector<int>& vec, int n){
+    int largest = vec[0];
+    for(int i = 0; i< n; i++){
+        if(vec[i] > largest){
+            largest = vec[i];
+        }
+    }
+    return largest;
+}
+
+//akhane second largest number dhore naua hoyeche -1. so when we compare it with a 
+//array number and if it greater than -1 and it is not equal to the largest then it will be
+// the second largest abong avabe array er last porjonto check korte thakte hobe 
+void sLargest(vector<int>& arr, int n){
+    int largest = largestf(arr, n);
+    int sLargest = -1;
+    for(int i = 0; i< n; i++){
+        if(arr[i] > sLargest && arr[i] != largest){
+            sLargest = arr[i];
+        }
+    } 
+    cout<< "The second largest number is: "<<sLargest<< endl;
+}
+
+// this is a bruteforce method and its time complexity is = O(2n)
+// but we can think a minimul approach. lets think:
+
+void secondLargest(vector<int>& vec, int n){
+    int largest = vec[0];
+    int seLargest = INT_MIN;
+    for(int i = 1; i< n; i++){
+        if(vec[i] > largest){
+            seLargest = largest;
+            largest  = vec[i];
+        }
+        else if(vec[i] < largest && vec[i] > seLargest){
+            seLargest = vec[i];
+        }
+    }
+    cout<< seLargest << endl;
+}
+
+
+
+
+int main()
+{
+    int n;
+    cin>> n;
+    vector<int> arr(n);
+    for( int i = 0; i<n; i++){
+        cin>> arr[i];
+    }
+    sLargest(arr, n);
+    secondLargest(arr, n);
+    return 0;
+}
+
