15565aa81b68ec0691b8bb428b04a353b49edf38
Md Rony Islam <mdronyislam149@gmail.com>
2025-06-29 16:00:36 +0600

    some changes

--- Arrays/removeDuplicate.cpp
+++ Arrays/removeDuplicate.cpp
@@ -23,3 +23,23 @@ int main()
     }
     return 0;
 }
+
+
+/*//minimal way
+int main(){
+    int n;
+    cin>> n;
+    vector<int> arr(n);
+    for(int i = 0; i<n; i++){
+        cin>> arr[i];
+    }
+    int i = 0;
+    for( int j = 1; j < n; j++){
+        if(arr[j] != arr[i]){
+            arr[i + 1] = arr[j];
+            i++;
+        }
+    }
+    return i+1;
+
+}*/
