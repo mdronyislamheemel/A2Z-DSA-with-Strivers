f4bb1528ebdebb5cf9409242d9eeea3235442863
Md Rony Islam <mdronyislam149@gmail.com>
2025-07-01 12:13:53 +0600

    intersection of two sorted array added

---
+++ Arrays/inserttionOfTwoSortedArray.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n1;
+    cin>> n1;
+    vector<int> a(n1);
+    for(int i = 0; i< n1; i++){
+        cin>> a[i];
+    }
+
+    int n2;
+    cin>> n2;
+    vector<int> b(n2);
+    for(int i = 0; i< n2; i++){
+        cin>> b[i];
+    }
+
+    //Brute force approach
+
+    //just take a array to store the intersection elements
+    vector<int> intersec;
+    vector<int> vis(n2, 0);
+    for(int i = 0; i < n1; i++){
+        for(int j = 0; j < n2; j++){
+            if(a[i] == b[j] && vis[j] == 0){
+                intersec.push_back(a[i]);
+                vis[j] = 1;
+                break;
+            }
+            if(b[j] > a[i]) break;
+        }
+    }
+    for(auto val : intersec){
+        cout<< val << " ";
+    }
+    return 0;
+}
