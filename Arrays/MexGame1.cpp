61e7ca4e625f1db6e7baaa351abb17f641972457
Md Rony Islam <mdronyislam149@gmail.com>
2025-07-02 20:11:23 +0600

    new things added

---
+++ Arrays/MexGame1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+using namespace std;
+int main()
+{
+    int t;
+    cin>> t;
+    while(t--){
+        int n;
+        cin>> n;
+        unordered_map<int, int> mp;
+        for(int i =0; i< n; i++){
+            int temp;
+            cin>> temp;
+            mp[temp]++;
+        }
+        for(int i = 0; ; i++){
+            cout<<i<<" "<<mp[i]<<endl;
+            if(mp[i] < i+1){
+                cout<< i<< endl;
+                break;
+            }
+            
+        }
+    
+
+    }
+    return 0;
+}
