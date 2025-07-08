eb153161eaf1f7c28e1f7b123b2e3dcd6f59e4f7
Md Rony Islam <mdronyislam149@gmail.com>
2025-07-08 12:59:57 +0600

    Kadane's algorithm added

--- Arrays/maximumSubarraySum.cpp
+++ Arrays/maximumSubarraySum.cpp
@@ -3,14 +3,7 @@
 using namespace std;
 
 // this is Brute force approach
-int main()
-{
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for(int i = 0; i< n; i++){
-        cin >> arr[i];
-    }
+int bruteForce(vector<int>& arr, int n){
     // st = start
     int maxSum = INT_MIN;
     for(int st = 0; st < n; st++){
@@ -20,6 +13,33 @@ int main()
             maxSum = max(maxSum, currSum );
         }
     }
-    cout << maxSum;
+    return maxSum;
+}
+
+//this is Kadanes algorithm
+int kadanes(vector<int> arr, int n){
+    int currSum = 0, maxSum = INT_MIN;
+    for(int i = 0; i<n; i++){
+        currSum += arr[i];
+        maxSum = max(maxSum, currSum);
+        if(currSum < 0){
+            currSum = 0;
+        }
+    }
+    return maxSum;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i< n; i++){
+        cin >> arr[i];
+    }
+    
+    cout << "According to bruteforce Max Subarray sum: "<< bruteForce(arr, n) << endl;
+    cout << "According to Kadane's algorithm Max Subarray sum: "<< kadanes(arr, n) << endl;
+
     return 0;
 }
