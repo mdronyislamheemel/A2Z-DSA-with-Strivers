ec11627eb04fd3babc355a7ff610beb7d911590d
Md Rony Islam <mdronyislam149@gmail.com>
2025-07-08 21:53:55 +0600

    new added

--- Arrays/maxWaterContainer.cpp
+++ Arrays/maxWaterContainer.cpp
@@ -8,10 +8,34 @@ long maxWater(vector<int>& height, int n){
     long area = 0;
     for(int i = 0; i<n; i++){
         for(int j = i+1; j<n; j++){
-            int width = j - 1;
+            int width = j - i;
             int ht = min(height[i], height[j]);
             area = width * ht;
             maxWater = max(maxWater, area);
+        }
+    }
+    return maxWater;
+    
+}
+
+//this would be optimize solution using two pointer
+long maxWaterTwoPointer( vector<int>& height, int n){
+    int left = 0, right = n-1;
+    long maxWater = 0;
+    while(left < right){
+        int width = right - left;
+        int ht = min(height[left], height[right]);
+        long area = width * ht;
+        maxWater = max(maxWater, area);
+        if(height[left] < height[right]){
+            left ++;
+        }
+        else if(height[left] > height[right]){
+            right --;
+        }
+        else {
+            left ++;
+            right--;
         }
     }
     return maxWater;
@@ -26,7 +50,8 @@ int main()
     for(int i = 0; i< n; i++){
         cin >> height[i];
     }   
-    cout << "Max Water: " << maxWater(height, n);
+    cout << "Max Water: " << maxWater(height, n) << endl;
+    cout << "Max Water using two pointer: " << maxWaterTwoPointer(height, n);
 
     return 0;
 }
