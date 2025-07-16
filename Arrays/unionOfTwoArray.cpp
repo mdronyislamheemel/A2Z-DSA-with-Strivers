07234b1884236aefb51816d0fff9151c82282dc0
Md Rony Islam <mdronyislam149@gmail.com>
2025-07-16 23:12:23 +0600

    creating function

--- Arrays/unionOfTwoArray.cpp
+++ Arrays/unionOfTwoArray.cpp
@@ -2,40 +2,28 @@
 #include<vector>
 #include<set>
 using namespace std;
-int main()
-{
-    int n1;
-    cin>> n1;
-    vector<int> a(n1);
-    for(int i = 0; i< n1; i++){
-        cin>> a[i];
+//Brute force approach
+void bruteForce(vector<int> a, vector<int> b, int n1, int n2){
+    set<int> st;
+    for(int i = 0; i <n1; i++){
+        st.insert(a[i]);
+    }
+    for(int i = 0; i <n2; i++){
+        st.insert(b[i]);
     }
 
-    int n2;
-    cin>> n2;
-    vector<int> b(n2);
-    for(int i = 0; i< n2; i++){
-        cin>> b[i];
+    vector<int> unon;
+    for(auto it : st){
+        unon.push_back(it);
     }
-    
-    // //brute force method
-    // set<int> st;
-    // for(int i = 0; i <n1; i++){
-    //     st.insert(a[i]);
-    // }
-    // for(int i = 0; i <n2; i++){
-    //     st.insert(b[i]);
-    // }
+    for( int val : unon){
+        cout << val<<" ";
+    }
+    cout << endl;
+}
 
-    // vector<int> unon;
-    // for(auto it : st){
-    //     unon.push_back(it);
-    // }
-    // for( int val : unon){
-    //     cout << val<<" ";
-    // }
-
-
+//minumul approach
+void minimulApproach(vector<int> a, vector<int> b, int  n1, int n2){
     //minimul approach ba 2 pointer approach
     int i = 0;
     int j = 0;
@@ -71,6 +59,26 @@ int main()
     for(int val : unionArr){
         cout<< val << " ";
     }
+}
 
+
+int main()
+{
+    int n1;
+    cin>> n1;
+    vector<int> a(n1);
+    for(int i = 0; i< n1; i++){
+        cin>> a[i];
+    }
+
+    int n2;
+    cin>> n2;
+    vector<int> b(n2);
+    for(int i = 0; i< n2; i++){
+        cin>> b[i];
+    }
+
+    bruteForce(a, b, n1, n2);
+    minimulApproach(a, b, n1, n2);   
     return 0;
 }
